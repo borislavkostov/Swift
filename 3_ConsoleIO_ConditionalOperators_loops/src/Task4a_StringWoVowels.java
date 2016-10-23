1800a363146841bf869ef951ef9d453ab7ccc877
borislavkostov <borislavkostov97@gmail.com>
2016-10-23 16:43:59 +0300

    Task_4a is ready and working

---
+++ 3_ConsoleIO_ConditionalOperators_loops/src/Task4a_StringWoVowels.java
@@ -0,0 +1,8 @@
+
+public class Task4a_StringWoVowels {
+	public static void main(String args[]){
+		String text = "Peter loves dogs";
+		String textf=text.replaceAll("[aeiou]","");
+		System.out.println(textf);
+	}
+}
