ceb0c8b53a9a10dbc5832f4b364e415fee4304ef
borislavkostov <borislavkostov97@gmail.com>
2016-10-23 16:36:14 +0300

    Task_4d is ready

--- 3_ConsoleIO_ConditionalOperators_loops/src/Task4d_SumOfNumbersInString.java
+++ 3_ConsoleIO_ConditionalOperators_loops/src/Task4d_SumOfNumbersInString.java
@@ -1,20 +1,18 @@
 
 public class Task4d_SumOfNumbersInString {
-	public static void main(String args[]){
-		String text = "Helen has 3 dogs and 11 cats";
-		String number[] = text.split("\\D");
-		int numbers[]=new int[number.length];
-		int i=0;
-		for (String st : number) {
-			System.out.println(st);
-			numbers[i]=Integer.parseInt(st);
-			i++;
-		}
+	public static void main(String args[]) {
+		String text = "Helen has 3 dogs, 11 cats and 3 pigs";
+		String number[] = text.split("\\D+");
+		int numbers[] = new int[number.length];
 		int sum = 0;
-		for (int num : numbers) {
-			System.out.println(num);
-			sum +=num;
+		for (int i = 1; i < number.length; i++) {
+			//System.out.println(number[i].trim());
+			numbers[i] = Integer.parseInt(number[i]);
+		}
+		for (int i = 0; i < numbers.length; i++) {
+			sum += numbers[i];
 		}
 		System.out.println(sum);
+
 	}
 }
