076162eb27bc8e79b1a6b22d9f5e75614ba1382c
borislavkostov <borislavkostov97@gmail.com>
2016-11-27 15:32:50 +0200

    Can't solve ^

---
+++ 10_DataStructures/Task9_ExpressionCalculator/src/Task9_ExpressionCalculator.java
@@ -0,0 +1,26 @@
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.script.ScriptEngine;
+
+public class Task9_ExpressionCalculator {
+	public static void main(String[] args) throws ScriptException {
+		Scanner input = new Scanner(System.in);
+		System.out.println(2^2);
+		String expression = input.nextLine();
+		List<Integer> index = new ArrayList<>();
+		for (int i = 0; i < index.size(); i++) {
+			
+		}
+		 expression.indexOf("^");
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByName("JavaScript");
+		String foo = expression;
+		System.out.println(engine.eval(foo));
+		
+	}
+}
