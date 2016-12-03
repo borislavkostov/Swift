3a4ccbc7cfc3d3705d3bdcd8952f5befb47de970
borislavkostov <borislavkostov97@gmail.com>
2016-12-03 12:30:06 +0200

    Task_0 aded PreparedStatement

--- 14_JDBC/Task0_Sakila/src/Task0_Sakila.java
+++ 14_JDBC/Task0_Sakila/src/Task0_Sakila.java
@@ -1,20 +1,32 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Task0_Sakila {
 	public static void main(String args[]) throws SQLException {
 		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/SwiftEvents", "root",
-				"*******")) {
-			try (Statement statement = conn.createStatement()) {
+				"******")) {
+			try (PreparedStatement statement = conn
+					.prepareStatement("INSERT INTO Events(`name`,`date`,`ticket_price`,`type_id`) VALUES(?,?,?,?)")) {
 				// statement.execute("INSERT INTO Events
 				// (`name`,`date`,`ticket_price`,`type_id`)VALUES('AC/DC','2016-10-12','50','2')");
 				// statement.execute("UPDATE `SwiftEvents`.`Events` SET
 				// `place`='Vasil Levski' WHERE `id`='7'");
-				statement.execute(
-						"INSERT INTO Events (`name`,`date`,`ticket_price`,`type_id`)VALUES('Rally da car','2017-05-3','300','5')");
+				// statement.execute(
+				// "INSERT INTO Events
+				// (`name`,`date`,`ticket_price`,`type_id`)VALUES('Rally da
+				// car','2017-05-3','300','5')");
+				statement.setString(1, "Neshka Robeva");
+				statement.setString(2, "2017-10-3");
+				statement.setDouble(3, 25.50);
+				statement.setInt(4, 5);
+				statement.execute();
+
 			}
+
 		}
 
 	}
