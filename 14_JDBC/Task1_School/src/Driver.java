2c52fff89ae7db82c24e2bb290bd6a35f2c3e353
borislavkostov <borislavkostov97@gmail.com>
2016-12-04 15:44:16 +0200

    Added Teacher class and changed method getTeacher()

--- 14_JDBC/Task1_School/src/Driver.java
+++ 14_JDBC/Task1_School/src/Driver.java
@@ -8,10 +8,13 @@ import java.sql.Statement;
 public class Driver {
 	public static void main(String args[]) throws SQLException {
 		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/School", "root", "173173");
-		try (ResultSet result = MySQLSchoolData.getStudent(3, conn);) {
-			while (result.next()) {
-				System.out.printf("%-30s| %s%n", result.getString("name"), result.getString("enrollment_date"));
-			}
-		}
+		//try (ResultSet result = MySQLSchoolData.getStudent(3, conn);) {
+			//while (result.next()) {
+				//System.out.printf("%-30s| %s%n", result.getString("name"), result.getString("enrollment_date"));
+			//}
+		
+		//}
+		Teacher tc =MySQLSchoolData.getTeacher(3, conn);
+		System.out.println(tc);
 	}
 }
--- 14_JDBC/Task1_School/src/MySQLSchoolData.java
+++ 14_JDBC/Task1_School/src/MySQLSchoolData.java
@@ -6,13 +6,21 @@ import java.sql.Statement;
 
 public class MySQLSchoolData {
 
-	public static ResultSet getTeacher(int id, Connection conn) throws SQLException {
-		PreparedStatement statement = conn.prepareStatement("SELECT * FROM School.teachers WHERE id=?;");
-		statement.setInt(1, id);
-		ResultSet result = statement.executeQuery();
-		return result;
+	public static Teacher getTeacher(int id, Connection conn) throws SQLException {
+		Teacher tc = null;
+		try (PreparedStatement statement = conn.prepareStatement("SELECT * FROM School.teachers WHERE id=?;")) {
+			statement.setInt(1, id);
+			try (ResultSet result = statement.executeQuery()) {
+				while (result.next()) {
+					tc = new Teacher(result.getInt("id"), result.getString("name"), result.getString("email"));
 
+				}
+				
+			}
+		}
+		return tc;
 	}
+
 	public static ResultSet getStudent(int id, Connection conn) throws SQLException {
 		PreparedStatement statement = conn.prepareStatement("SELECT * FROM School.Students WHERE id=?;");
 		statement.setInt(1, id);
