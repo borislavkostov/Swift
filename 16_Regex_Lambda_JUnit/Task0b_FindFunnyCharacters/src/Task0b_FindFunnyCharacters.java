b4b9a875d4c339ce104486af4e26f403406c190b
borislavkostov <borislavkostov97@gmail.com>
2016-12-11 11:46:54 +0200

    Task_0b is ready

---
+++ 16_Regex_Lambda_JUnit/Task0b_FindFunnyCharacters/src/Task0b_FindFunnyCharacters.java
@@ -0,0 +1,13 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task0b_FindFunnyCharacters {
+	public static void main(String args[]) {
+		String text = "❅❅❅☎☎☎♚♚tieosf❅❅☭☭☎☎♚♚♚☎☎☮♚♚☮❅☠rewrwgv☠☠❅❅☎☎☎☮☮☠☠☭☭♚♚❅❅❅☮☮☮☭☭☭❅❅☮☠☠☠☮☮☮❅❅☮☭☭☮☮☭";
+		Pattern pattern = Pattern.compile("[^a-zA-z0-9~!@#\\$%\\^&\\*\\(\\)_\\+-\\=/\\\\)]");
+		Matcher matcher = pattern.matcher(text);
+		while (matcher.find()) {
+			System.out.printf("The funny symbol is [%s]%n",matcher.group());
+		}
+	}
+}
