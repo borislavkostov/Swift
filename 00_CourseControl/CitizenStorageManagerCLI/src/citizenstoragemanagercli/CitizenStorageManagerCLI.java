3e91a8bf4e18d7455e48180174c89e25b2fff4dd
borislavkostov <borislavkostov97@gmail.com>
2016-12-16 23:26:35 +0200

    Now can enter person and address in one method

--- 00_CourseControl/CitizenStorageManagerCLI/src/citizenstoragemanagercli/CitizenStorageManagerCLI.java
+++ 00_CourseControl/CitizenStorageManagerCLI/src/citizenstoragemanagercli/CitizenStorageManagerCLI.java
@@ -1,21 +1,34 @@
-
 package citizenstoragemanagercli;
 
 import MySQL.MySqlAddressStorage;
 import java.sql.SQLException;
 import address.Address;
-
+import MySQL.MySQLPersonStorage;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+import personaldetails.Citizen;
+import personaldetails.Gender;
 
 public class CitizenStorageManagerCLI {
 
-    public static void main(String[] args) throws SQLException{
-        MySqlAddressStorage adr = new  MySqlAddressStorage();
+    public static void main(String[] args) throws SQLException {
+        MySqlAddressStorage adr = new MySqlAddressStorage();
         //adr.setCountry("USA");
         //System.out.println(adr.setCountry("USA"));
-        Address adres = new Address("Bulgaria","Ivanovgrad","Varna","1750","jerusalim","3",2,1);
-        adr.insertAddress(adres);
-        Address adresa = new Address("USA","Wachington","Sofia-grad","1780","blok","222");
-        adr.insertSmallAddress(adresa);
+//        Address adres = new Address("Bulgaria","Ivanovgrad","Varna","1750","jerusalim","3",2,1);
+//        adr.insertAddress(adres);
+//        Address adresa = new Address("USA","Wachington","Sofia-grad","1780","blok","222");
+//        adr.insertSmallAddress(adresa);
+        MySQLPersonStorage personStorage = new MySQLPersonStorage();
+        DateTimeFormatter germanFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(Locale.GERMAN);
+        LocalDate date = LocalDate.parse("13.03.1997",germanFormatter); 
+        Citizen person = new Citizen("Borislav","Ivailov","Kostov",Gender.Male,198,date);
+        Address adres = new Address("Bulgaria","Sofia","Sofia-grad","1780","jerusalim","13",4,22);
+        //System.out.println(personStorage.enterPerson(person));
+        personStorage.enterAddressID(personStorage.enterPerson(person),adr.insertAddress(adres));
+        
     }
-    
+
 }
