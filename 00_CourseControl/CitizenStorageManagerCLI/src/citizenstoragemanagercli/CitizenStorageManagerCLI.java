ac262395abea966289287781e727dcbb0bfbe7d2
borislavkostov <borislavkostov97@gmail.com>
2016-12-26 15:00:01 +0200

    Now can enter person and address and educations in one method

--- 00_CourseControl/CitizenStorageManagerCLI/src/citizenstoragemanagercli/CitizenStorageManagerCLI.java
+++ 00_CourseControl/CitizenStorageManagerCLI/src/citizenstoragemanagercli/CitizenStorageManagerCLI.java
@@ -10,7 +10,9 @@ import java.time.format.FormatStyle;
 import java.util.Locale;
 import personaldetails.Citizen;
 import personaldetails.Gender;
-
+import education.*;
+import java.util.ArrayList;
+import java.util.List;
 public class CitizenStorageManagerCLI {
 
     public static void main(String[] args) throws SQLException {
@@ -23,10 +25,16 @@ public class CitizenStorageManagerCLI {
 //        adr.insertSmallAddress(adresa);
         MySQLPersonStorage personStorage = new MySQLPersonStorage();
         DateTimeFormatter germanFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(Locale.GERMAN);
-        LocalDate date = LocalDate.parse("13.03.1997", germanFormatter);
-        Citizen person = new Citizen("Alexander", "Ivailov", "Kostov", Gender.Male, 198, date);
+        LocalDate date = LocalDate.parse("13.03.1973", germanFormatter);
+        Citizen person = new Citizen("Petar", "Borisov", "Kostov", Gender.Male, 198, date);
         Address adres = new Address("Bulgaria", "Sofia", "Sofia-grad", "1780", "jerusalim", "13", 4, 22);
         person.setAddress(adres);
+        List<Education> education= new ArrayList<>();
+        Education edu = new SecondaryEducation("PGTK",LocalDate.parse("13.09.2000", germanFormatter),LocalDate.parse("24.05.2005", germanFormatter));
+        edu.isGraduated();
+        Education edu1 = new HigherEducation("UNWE",LocalDate.parse("15.09.2005", germanFormatter),LocalDate.parse("20.05.2011", germanFormatter),EducationDegree.Bachelor);
+        person.addEducation(edu1);
+        person.addEducation(edu);
         personStorage.enterPerson(person);
 
     }
